8d8042071892b4278ff100dd49621f17825a321c
asmitamohanty <asmitamohanty13@gmail.com>
2025-10-17 16:56:29 -0700

    update working connector.py => push success to bigq

--- rag_devpost_hack/connector.py
+++ rag_devpost_hack/connector.py
@@ -1,5 +1,6 @@
-from fivetran_connector_sdk import Connector
+from fivetran_connector_sdk import Connector, Operations as op
 import requests
+from datetime import datetime
 # import json
 
 # with open("configuration.json", "r") as file:
@@ -9,12 +10,12 @@ table_config = [
   {
     "table": "disasters",
     "columns": {
-      "id": "INT",
+      "id": "STRING",
       "title": "STRING",
-      "date": "UTC_DATETIME",
-    "type": "STRING",
-    "country": "STRING",
-    "status": "STRING"
+      "date": "STRING",
+      "country": "STRING",
+      "type": "STRING",
+      "status": "STRING"
     }
   }
 ]
@@ -23,7 +24,7 @@ class Disaster(Connector):
     @staticmethod
     def schema(configuration):
         return table_config
-    
+
     @staticmethod
     def update(configuration, state):
         # url = config.get("api_url")
@@ -41,22 +42,32 @@ class Disaster(Connector):
             "sort[]": "date:desc"
         }
 
-        data = requests.get(url, params=params).json().get("data", [])
+        data_ = requests.get(url, params=params).json().get("data", [])
 
-        records = []
-        for item in data:
+        #records = []
+        for item in data_:
             fields = item["fields"]
-            records.append({
+            record={
                 "id": item.get("id"),
                 "title": fields.get("name"),
                 "date": fields.get("date").get("changed"),
                 "country": (fields.get("country") or [{}])[0].get("name") if fields.get("country") else None,
                 "type": fields.get("primary_type", {}).get("name"),
                 "status":fields.get("status")
-            })
-
-        return {"disasters": records}
-
+            }
+            op.upsert(table="disasters", data=record)
+        #     data={
+        #         "id": "1",
+        #         "title": "test_record",
+        #         "date": "2025-10-15",
+        #         "country": "US",
+        #         "type": "test",
+        #         "status": "active"
+        #   }
+        # )
+        new_state = {"last_sync_timestamp": datetime.utcnow().isoformat()}
+        op.checkpoint(state=new_state)
+        
 _disaster_connector = Disaster(update={})
 connector = Connector(
     update=_disaster_connector.update,
